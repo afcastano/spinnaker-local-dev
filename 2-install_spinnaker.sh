43a42a72eeb8ee0d2b20bd31932b78ee15253064
Andres Castano <andres@3wks.com.au>
2019-10-26 00:47:40 +1100

    clean

--- 2-install_spinnaker.sh
+++ 2-install_spinnaker.sh
@@ -3,5 +3,6 @@ helm fetch stable/spinnaker --version=1.16.1
 echo templating helm chart
 helm template --name spin --namespace spin  --values values.yaml spinnaker-1.16.1.tgz > spinnaker.yaml
 echo applying chart
+kubectl --kubeconfig k3s.yaml create namespace spin
 kubectl --kubeconfig k3s.yaml apply -f spinnaker.yaml -n spin
 kubectl --kubeconfig k3s.yaml get pods -n spin
