7e92afb1d817672146a3003867b2911402775031
Andres Castano <andres@3wks.com.au>
2019-10-31 13:58:40 +1100

    wip

--- 1-install_k3s.sh
+++ 1-install_k3s.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
+cyan='\033[36m'
+white="\033[1m"
+reset='\033[0m'
+clusterName='spin-k3s'
+clusterCount=$(k3d l | grep k3s-default | wc -l)
+echo $clusterCount
+if [ $clusterCount = 1 ]
+then
+    echo "k3d cluster already exist..."
+else
+    echo "Creating k3d cluster..."    
+    k3d create --image rancher/k3s:v0.9.1
+fi
 
-echo "Launching vm k3s-spin"
-multipass launch --name k3s-spin --cpus 4 --mem 8G --disk 20G
-echo "Installing k3s"
-multipass exec k3s-spin -- sh -c "curl -sfL https://get.k3s.io/ | INSTALL_K3S_VERSION=v0.9.1 sh -"
-echo "Copying kube config"
-multipass exec k3s-spin -- sh -c "sudo cp /etc/rancher/k3s/k3s.yaml . && sudo chmod a=r k3s.yaml"
-multipass copy-files k3s-spin:k3s.yaml k3s.yaml
-echo "Updating kube config"
-K3S_IP=$(multipass info k3s-spin --format json | jq -r '.info."k3s-spin".ipv4[0]')
-sed -i -e "s/127.0.0.1/${K3S_IP}/g" k3s.yaml
-rm k3s.yaml-e
-echo "Setting up local storage"
-curl -LO  https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
-echo "Apply local storage"
-kubectl --kubeconfig k3s.yaml apply -f local-path-storage.yaml
-sleep 5
-kubectl --kubeconfig k3s.yaml patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-rm local-path-storage.yaml
 echo "Following you should see all installed resources"
-kubectl --kubeconfig k3s.yaml get all --all-namespaces
+KUBECONFIG="$(k3d get-kubeconfig --name='k3s-default')"
+kubectl --kubeconfig=$KUBECONFIG get all --all-namespaces
+RESULT=$?
+if [ $RESULT -eq 0 ]
+then
+    echo "----------"
+    echo "k3s configured correctly, configure your kubectl by running:"
+    echo "${white}export KUBECONFIG=${cyan}\$(k3d get-kubeconfig --name='k3s-default')${reset}"
+    exit 0
+fi
+echo "Error configuring k3s"
+exit 1
--- 2-install_spinnaker.sh
+++ 2-install_spinnaker.sh
@@ -3,6 +3,7 @@ helm fetch stable/spinnaker --version=1.16.1
 echo templating helm chart
 helm template --name spin --namespace spin  --values values.yaml spinnaker-1.16.1.tgz > spinnaker.yaml
 echo applying chart
-kubectl --kubeconfig k3s.yaml create namespace spin
-kubectl --kubeconfig k3s.yaml apply -f spinnaker.yaml -n spin
-kubectl --kubeconfig k3s.yaml get pods -n spin
+KUBECONFIG="$(k3d get-kubeconfig --name='k3s-default')"
+kubectl create namespace spin
+kubectl apply -f spinnaker.yaml -n spin
+kubectl get pods -n spin
